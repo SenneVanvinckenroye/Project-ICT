13e1a87213e18884c1e6c6ecf2f1843468308db3
DriesCrauwels <dries.crauwels@student.artesis.be>
2013-03-07 10:35:54 +0100

    Versie Rien

---
+++ src/Project ICT 4 - Medicine Planner/MedAgent 0.1/MedAgent 0.1/www/js/main.js
@@ -0,0 +1,24 @@
+﻿/* File Created: maart 6, 2013 */
+
+
+$(document).ready(function () {//Onload jquery func
+    // Handler for .ready() called.
+    $(".login").hide(); //hide elementen in klasse 'page2'
+    //change input field html code
+});
+
+function gotoField(veld) {
+    switch(veld) 
+    {
+        case'login':
+        {
+            $(".login").show(); //toon elementen in klasse 'page2'
+            //change input field html code
+        }
+        case'register':
+        {
+            $(".register").show(); //toon elementen in klasse 'page2'
+        }
+    }
+    
+}
