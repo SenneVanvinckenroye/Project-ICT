082978f1341ac85baf0a61b515dbf8ebe60f61fe
DriesCrauwels <dries.crauwels@student.artesis.be>
2013-03-07 13:12:34 +0100

    First Upload

---
+++ src/Project ICT 4 - Medicine Planner (C#)/MedAgent 0.1/MedAgent 0.1/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace MedAgent_0_1
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PatientButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        	// TODO: Add event handler implementation here.
+        }
+
+    }
+	
+	
+}
