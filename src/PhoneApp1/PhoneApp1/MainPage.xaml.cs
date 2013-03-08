2e5e252c5986b1e895c8c2d20825dd7eb227a36a
senne.vanvinckenroye@gmail.com <senne.vanvinckenroye@gmail.com>
2013-03-08 19:46:41 +0100

    nieuw wp7 project. kan met azure sql db communiceren

---
+++ src/PhoneApp1/PhoneApp1/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Newtonsoft.Json.Linq;
+
+namespace PhoneApp1
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private static MobileServiceTable<TodoItem> todoItemTable = App.MobileServiceClient.GetTable<TodoItem>();
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            Service.Read();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Service.Insert(txtbox.Text);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Service.Read();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            lstbox.Items.Clear();
+            foreach (TodoItem todoItem in Service.Load())
+            {
+                lstbox.Items.Add(todoItem);
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Service.Delete((TodoItem)lstbox.SelectedItem);
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            Service.Query(txtbox.Text);
+        }
+
+
+
+
+    }
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+}
---
+++ src/PhoneApp1/PhoneApp1/Service.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PhoneApp1
+{
+    class Service
+    {
+
+        private static MobileServiceTable<TodoItem> todoItemTable = App.MobileServiceClient.GetTable<TodoItem>();
+
+        public static void Insert(string data)
+        {
+            var item = new TodoItem { Text = data };
+            todoItemTable.Insert(item, (res, err) =>
+            {
+                if (err != null)
+                {
+                    //handle it
+                    System.Diagnostics.Debugger.Break();
+                    return;
+                }
+                item = res;
+            });
+        }
+
+        public static List<TodoItem> lst;
+
+        public static void Read()
+        {
+            // OData query
+            lst = new List<TodoItem>();
+            todoItemTable.GetAll((res, err) =>
+            {
+                if (err != null)
+                {
+                    //handle it
+                    System.Diagnostics.Debugger.Break();
+                    return;
+                }
+                foreach (TodoItem todo in res)
+                {
+                    lst.Add(todo);
+                }
+            });
+        }
+        public static List<TodoItem> Load()
+        {
+            if (lst != null)
+            {
+                return lst;
+            }
+            else
+            {
+                System.Diagnostics.Debugger.Break();
+                return null;
+            }
+        }
+        public static void Delete(TodoItem item)
+        {
+            todoItemTable.Delete(item.Id, err =>
+            {
+                if (err != null)
+                {
+                    //handle it
+                    System.Diagnostics.Debugger.Break();
+                }
+            });
+        }
+        public static void Query(string query1)
+        {
+            // OData query http://www.odata.org/documentation/uri-conventions#FilterSystemQueryOption
+            lst = new List<TodoItem>();
+            var query = new MobileServiceQuery()
+                .Filter(query1);
+
+            todoItemTable.Get(query, (res, err) =>
+            {
+                if (err != null)
+                {
+                    //handle it
+                    System.Diagnostics.Debugger.Break();
+                    return;
+                }
+                foreach (TodoItem todo in res)
+                {
+                    lst.Add(todo);
+                }
+            });
+        }
+    }
+}
