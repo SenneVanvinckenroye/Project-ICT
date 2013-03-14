16ec1fb7771dbbccb038d1677f9716fedea02712
rienvb <rien.vanbeek@student.artesis.be>
2013-03-14 12:21:47 +0100

    slider werkt

--- src/Project ICT 4 - Medicine Planner (C#)/MedAgent 0.1/MedAgent 0.1/AddMedPlanPage.xaml.cs
+++ src/Project ICT 4 - Medicine Planner (C#)/MedAgent 0.1/MedAgent 0.1/AddMedPlanPage.xaml.cs
@@ -19,5 +19,26 @@ namespace MedAgent_0_1
         {
             InitializeComponent();
         }
+
+        private void course_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            switch ((int)(course.Value))
+            {
+                case 0: repeatInfo_txt.Text = "Daily";
+                    break;
+                case 1: repeatInfo_txt.Text = "Every 2 days";
+                    break;
+                case 2: repeatInfo_txt.Text = "Every 3 days";
+                    break;
+                case 3: repeatInfo_txt.Text = "Every 4 days";
+                    break;
+                case 4: repeatInfo_txt.Text = "Every 5 days";
+                    break;
+                case 5: repeatInfo_txt.Text = "Weekly";
+                    break;
+                default: repeatInfo_txt.Text = "error";
+                    break;
+            }
+        }
     }
 }
